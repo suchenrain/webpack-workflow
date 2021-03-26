f47312e736a7fd691e53976794019f423d37c066
suchenrain <suchenxiaoyu@qq.com>
2021-03-26 19:35:16 +0800

    webpack start

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import './style.css';
+
+import data from './data.json';
+
+console.log('data :>> ', data);
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log('add(1,2) :>> ', add(1, 2));
